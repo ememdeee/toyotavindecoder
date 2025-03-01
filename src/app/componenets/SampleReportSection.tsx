16e25b7670d4af6019c1660c02073ff8fd556152
ememdeee <basurah.muhammad.mb@gmail.com>
2025-03-02 01:02:45 +0700

    ready for ahref review

--- src/app/componenets/SampleReportSection.tsx
+++ src/app/componenets/SampleReportSection.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
 import CustomButton from "./CustomButton"
 
 interface SampleReportSectionProps {
